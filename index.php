8a50772d1cd22200df93d49d3f9d6b26282d6fbd
freek <freek@spatie.be>
2021-05-31 20:25:31 +0200

    wip

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
 
 use App\Test;
-use Spatie\FlareClient\Flare;
-use Spatie\FlareClient\FlareMiddleware\AddGitInformation;
 use Spatie\Ignition\Ignition;
 
 include 'vendor/autoload.php';
 
 Ignition::make()
     ->applicationPath(__DIR__)
-    ->registerMiddleware(AddGitInformation::class)
     ->register();
-
 
 (new Test)->myPropertyy;
 
